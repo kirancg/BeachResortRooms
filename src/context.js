cb7f04c93bac919d5667c9a276b1975e6750ec15
Kiran_Kumar_Gangadharaiah <gangadharaiah.k@husky.neu.edu>
2020-05-04 13:29:27 -0400

    Added size filter

--- src/context.js
+++ src/context.js
@@ -95,6 +95,9 @@ class RoomProvider extends Component {
         //filter by price
         tempRooms = tempRooms.filter(room => room.price <= price);
 
+        //filter by size
+        tempRooms = tempRooms.filter(room => room.size >= minSize && room.size<=maxSize);
+
         this.setState({
             sortedRooms: tempRooms
         })
